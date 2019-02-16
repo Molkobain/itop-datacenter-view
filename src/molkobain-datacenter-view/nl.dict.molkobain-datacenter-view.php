76ebf32597a847fe3e7087e6b01de9a1e116eafb
Molkobain <lajarige.guillaume@free.fr>
2019-02-16 20:10:15 +0100

    :globe_with_meridians: Update NL translation

--- src/molkobain-datacenter-view/nl.dict.molkobain-datacenter-view.php
+++ src/molkobain-datacenter-view/nl.dict.molkobain-datacenter-view.php
@@ -33,7 +33,7 @@ Dict::Add('NL NL', 'Dutch', 'Nederlands', array(
 	// - Rack
 	'Molkobain:DatacenterView:Rack:Panel:Front:Title' => 'Voorkant',
 	// - Enclosure
-	'Molkobain:DatacenterView:Enclosure:Panel:Front:Title' => 'Front~~',
+	'Molkobain:DatacenterView:Enclosure:Panel:Front:Title' => 'Voorkant',
 	// - Unmounted elements
 	'Molkobain:DatacenterView:Unmounted:Enclosures:Title' => 'Ongemonteerde behuizingen',
 	'Molkobain:DatacenterView:Unmounted:Enclosures:Title+' => 'Horend bij dit rack, maar positie nog niet bepaald (bewerk de behuizing om dit te doen)',
